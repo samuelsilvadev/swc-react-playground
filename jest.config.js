98ea0789f692e11de306bcc6bd4c3cba4fbd6bfe
Samuel Silva <samuelsilvawb@gmail.com>
2021-07-27 22:28:03 +0100

    chore: Add module alias to jest environment

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,10 @@ module.exports = {
     ".js$": ["@swc/jest"],
   },
   setupFilesAfterEnv: ["./tests/jest-setup.js"],
+  moduleNameMapper: {
+    "^config/(.*)$": "<rootDir>/src/config/$1",
+    "^components/(.*)$": "<rootDir>/src/components/$1",
+    "^hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^services/(.*)$": "<rootDir>/src/services/$1",
+  },
 };
