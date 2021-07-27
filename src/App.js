6a13a0efc700805a186bf64a691375dd562a85c8
Samuel Silva <samuelsilvawb@gmail.com>
2021-07-27 07:26:21 +0100

    feat: Structure relative fontSize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { styled } from "@stitches/react";
+import { styled, global } from "@stitches/react";
 
 const H1 = styled("h1", {
   color: "red",
 });
 
+const createGlobalStyles = global({
+  "*": {
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+  },
+  html: {
+    fontSize: "62.5%",
+  },
+  body: {
+    fontSize: "1.6rem",
+  },
+});
+
 function App() {
+  createGlobalStyles();
+
   return <H1>App 1</H1>;
 }
 
