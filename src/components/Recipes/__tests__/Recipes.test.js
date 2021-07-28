bb9131e11914af36d4672e5994ce5f40b5abacfd
Samuel Silva <samuelsilvawb@gmail.com>
2021-07-28 18:35:49 +0100

    test: Add unit test to recipes component

---
+++ src/components/Recipes/__tests__/Recipes.test.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import useRecipes from "hooks/api/useRecipes";
+
+import Recipes from "../Recipes";
+
+jest.mock("hooks/api/useRecipes");
+
+describe("<Recipes />", () => {
+  it("renders list of recipes", () => {
+    useRecipes.mockImplementationOnce(() => ({
+      recipes: [
+        {
+          id: "id",
+          name: "recipe-name",
+          description: "recipe-description",
+        },
+      ],
+    }));
+
+    render(<Recipes />);
+
+    expect(screen.getByText("recipe-name")).toBeVisible();
+    expect(screen.getByText("recipe-description")).toBeVisible();
+  });
+
+  it("renders loading indicator", () => {
+    useRecipes.mockImplementationOnce(() => ({
+      loading: true,
+    }));
+
+    render(<Recipes />);
+
+    expect(screen.getByText(/Loading list of recipes/)).toBeVisible();
+  });
+
+  it("renders error message", () => {
+    useRecipes.mockImplementationOnce(() => ({
+      error: "error",
+    }));
+
+    render(<Recipes />);
+
+    expect(
+      screen.getByText(/Something went wrong while loading recipes/)
+    ).toBeVisible();
+  });
+});
