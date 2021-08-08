c305e78be8cb5a6fd71f5d520f913b7eb8d1ad57
Samuel Silva <samuelsilvawb@gmail.com>
2021-08-08 20:56:35 +0100

    feat: Add Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { styled } from "@stitches/react";
+import PropTypes from "prop-types";
+
+const ButtonStyled = styled("button", {
+  textTransform: "uppercase",
+  background: "transparent",
+  border: "0.1rem solid #000",
+  cursor: "pointer",
+  padding: "1rem 2rem",
+});
+
+function Button(props) {
+  const { children, type = "button", className, ...remainingProps } = props;
+
+  return (
+    <ButtonStyled className={className} type={type} {...remainingProps}>
+      {children}
+    </ButtonStyled>
+  );
+}
+
+Button.propTypes = {
+  type: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+};
+
+export default Button;
